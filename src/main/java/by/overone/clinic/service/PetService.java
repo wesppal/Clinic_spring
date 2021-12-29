963a670bc5f40e08676937a725ecea8359142b43
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-12-29 03:42:19 +0300

    create pet in service

---
+++ src/main/java/by/overone/clinic/service/PetService.java
@@ -0,0 +1,19 @@
+package by.overone.clinic.service;
+
+import by.overone.clinic.model.Pet;
+
+import java.util.List;
+
+public interface PetService {
+    List<Pet> getPets();
+
+    Pet getPetById(long id);
+
+    Pet addPet(long user_id, Pet pet);
+
+    void updatePet(long id, Pet pet);
+
+    boolean deletePet(long id);
+
+    List<Pet> getPetByUserId(long user_id);
+}
---
+++ src/main/java/by/overone/clinic/service/impl/PetServiceImpl.java
@@ -0,0 +1,38 @@
+package by.overone.clinic.service.impl;
+
+import by.overone.clinic.model.Pet;
+import by.overone.clinic.service.PetService;
+
+import java.util.List;
+
+public class PetServiceImpl implements PetService {
+    @Override
+    public List<Pet> getPets() {
+        return null;
+    }
+
+    @Override
+    public Pet getPetById(long id) {
+        return null;
+    }
+
+    @Override
+    public Pet addPet(long user_id, Pet pet) {
+        return null;
+    }
+
+    @Override
+    public void updatePet(long id, Pet pet) {
+
+    }
+
+    @Override
+    public boolean deletePet(long id) {
+        return false;
+    }
+
+    @Override
+    public List<Pet> getPetByUserId(long user_id) {
+        return null;
+    }
+}
