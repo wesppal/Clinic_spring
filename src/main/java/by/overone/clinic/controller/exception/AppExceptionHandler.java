fb84d50cd6efb3e1b5c2ed23a9eab923e61e1174
Pavel Ivanchik <ivanchik.pal@gmail.com>
2022-01-13 23:31:06 +0300

    fix

--- src/main/java/by/overone/clinic/controller/exception/AppExceptionHandler.java
+++ src/main/java/by/overone/clinic/controller/exception/AppExceptionHandler.java
@@ -4,7 +4,6 @@ import by.overone.clinic.exception.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
